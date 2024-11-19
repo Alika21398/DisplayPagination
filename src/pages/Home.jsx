7ffade6f217f116a2243db7e38640228cb8ba74a
Alika <dangolalika2017@gmail.com>
2024-11-19 23:02:05 +0545

    modified home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,29 +3,49 @@ import { useApi } from "../App";
 
 const Home = () => {
   const { data } = useApi();
-
+  const [selectedRange, setSelectedRange] = useState(5);
   const [selectedData, setSelectedData] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
 
   useEffect(() => {
     // used this to display data at first without selecting any range
     // data dependecines so that everytime data is chnaged it displaysdata and useeffect is runs. Used data dependecy because at first nothing was displayed as i assumed data wasn't fetched so used dependency
-    const filteredData = data.filter((x) => x.id >= 1 && x.id <= 10);
+    const filteredData = data.slice(0, selectedRange);
     setSelectedData(filteredData);
     console.log("displayData", filteredData);
-  }, [data]);
+  }, [data, selectedRange]);
 
-  const handleChange = (range) => {
-    const rangeArray = range.split("-");
-    const initialValue = rangeArray[0];
-    const finalValue = rangeArray[1];
-    console.log("rangeArray", rangeArray);
+  // to find the total page required
+  const totalPages = Math.ceil(data.length / selectedRange);
+  console.log("totalPages", totalPages);
 
-    const filteredData = data.filter(
-      (x) => x.id >= initialValue && x.id <= finalValue
-    );
+  // to choose how many to display on a page
+  const handleRangeChange = (e) => {
+    const rangeValue = parseInt(e.target.value, 10);
+    setSelectedRange(rangeValue);
+    setCurrentPage(1);
+    const startIndex = 0;
+    const endIndex = rangeValue;
+    setSelectedData(data.slice(startIndex, endIndex));
 
-    setSelectedData(filteredData);
-    console.log("filteredData", filteredData);
+    console.log("rangeValue", rangeValue);
+  };
+  //  for making a dropdown dynamic means the numbers will be set according to length od data in difference of 5
+  const dropdownOption = () => {
+    const options = [];
+    const maxItem = data.length;
+    for (let i = 5; i <= maxItem; i += 5) {
+      options.push(i);
+    }
+    console.log("options", options);
+    return options;
+  };
+  // pagination for changing page previous and next
+  const handlePageChange = (page) => {
+    setCurrentPage(page);
+    const startIndex = (page - 1) * selectedRange;
+    const endIndex = startIndex + selectedRange;
+    setSelectedData(data.slice(startIndex, endIndex));
   };
 
   console.log("select", selectedData);
@@ -33,21 +53,48 @@ const Home = () => {
   return (
     <>
       <div className="px-44 py-20">
-        <label htmlFor="">Page number</label>
-        <select
-          className="border ml-3 p-2 bg-blue-100"
-          name=""
-          id=""
-          onChange={(e) => {
-            handleChange(e.target.value);
+        <div className="flex justify-between">
+          <div>
+            <label htmlFor="">Page number</label>
+            <select
+              className="border ml-3 p-2 bg-blue-100"
+              name=""
+              id=""
+              value={selectedRange}
+              onChange={
+                handleRangeChange
 
-            console.log("e", e.target.value);
-          }}
-        >
-          <option value="1-10"> 1-10</option>
-          <option value="11-20">11-20</option>
-          <option value="21-30">21-30</option>
-        </select>
+                // console.log("e", e.target.value);
+              }
+            >
+              {dropdownOption().map((option) => (
+                <option key={option} value={option}>
+                  {" "}
+                  {option}
+                </option>
+              ))}
+            </select>
+          </div>
+
+          {/* pagination for paging  */}
+          <div className="flex gap-5  justify-end items-center">
+            <button
+              onClick={() => handlePageChange(currentPage - 1)}
+              disabled={currentPage == 1}
+            >
+              prev
+            </button>
+            <div>{currentPage}</div>
+            <button
+              onClick={() => handlePageChange(currentPage + 1)}
+              disabled={currentPage == totalPages}
+            >
+              Next
+            </button>
+          </div>
+        </div>
+
+        {/* displaying data  */}
 
         {selectedData.map((item, index) => {
           return (
